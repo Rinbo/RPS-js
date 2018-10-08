0bd4e69733e77f6fdb3f8100b208203d7b771e01
rinbo <robin.b@outlook.com>
2018-10-08 22:39:48 +0200

    Add logic that displays who won the game and if player wants to play again

--- src/app.js
+++ src/app.js
@@ -10,7 +10,13 @@ global.playerScore = document.getElementById('player-score');
 global.opponentScore = document.getElementById('opponent-score');
 global.gameBoard = document.getElementById('game-board')
 global.playerLabel = document.getElementById('player-score-label');
+global.opponentLabel = document.getElementById('opponent-score-label');
 global.roundCountLabel = document.getElementById('round-count');
+global.yesButton = document.getElementById('yes');
+global.noButton = document.getElementById('no');
+global.endGame = document.getElementById('end-game');
+global.game = document.getElementById('game');
+global.resultText = document.getElementById('result-text');
 const t = "tie";
 const w = "win";
 const l= "lose";
@@ -18,23 +24,18 @@ global.resultMatrix = [[t,w,l],[l,t,w],[w,l,t]];
 let playerScore = 0;
 let opponentScore = 0;
 global.roundCount = 0;
-
-
+let numberOfRounds = 5;
+let opponents = ["Benny the Butcher", "Gerry the Greek", "Ronny the Ruler", "Thormorthur the Viking"]
 
 document.addEventListener('DOMContentLoaded', () => {
   
   global.beginButton.addEventListener('click', event => {
     if (global.inputField.value) {global.playerLabel.innerText = global.inputField.value};
+    global.opponentLabel.innerText = opponents[Math.floor(Math.random() * 4)]
     global.gameBoard.style.display = "block";
     global.beginButton.style.display = "none";
     global.inputField.style.display = "none";
   });
-
-  global.roundCountLabel.addEventListener('onchange', e => {
-    if (global.roundCount === 10) {
-      endGame();
-    }
-  })
 
   global.rock.addEventListener('click', event => {
     let opponentChoice = getOpponentChoice();
@@ -60,14 +61,15 @@ document.addEventListener('DOMContentLoaded', () => {
       opponentScore += 1;
       global.opponentScore.innerText = opponentScore;
     }
-    setTimeout(clearOpView, 1000);
-    global.roundCount += 1;
-    global.roundCountLabel.innerText= global.roundCount;
-    if (global.roundCount == 10) {endGame()}; 
+    
+    setTimeout(clearOpView, 1600);     
   }
 
   function clearOpView() {
     global.opponentChoice.classList = "";
+    global.roundCount += 1;
+    global.roundCountLabel.innerText= global.roundCount;    
+    if (global.roundCount === numberOfRounds) {endGame()};
   }
 
   function getOpponentChoice() {
@@ -85,9 +87,49 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function endGame() {
-    alert("Game Ended!");
+    let winnerText = winner();
+    wait(1000);
+    global.game.style.display = "none";
+    global.endGame.style.display= "block";
+    global.resultText.innerText = winnerText;    
     global.roundCount = 0;
     global.roundCountLabel.innerText = 0;
+    opponentScore = 0;
+    playerScore = 0;
+    global.opponentScore.innerText = 0;
+    global.playerScore.innerText = 0;
   }
+
+  global.yesButton.addEventListener('click', () => {
+    global.game.style.display = "block";
+    global.endGame.style.display= "none";        
+  })
+
+  global.noButton.addEventListener('click', () => {
+    global.game.style.display = "block";
+    global.endGame.style.display= "none";
+    global.gameBoard.style.display = "none";
+    global.beginButton.style.display = "block";
+    global.inputField.style.display = "block";
+
+  })
+
+  function winner() {
+    if (playerScore > opponentScore) {
+      return `${global.inputField.value} won!`
+    } else if (opponentScore > playerScore) {
+      return `${global.opponentLabel.innerText} won :(.` 
+    } else {
+      return "The game ended in a tie."
+    }
+  }
+
+  function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+      end = new Date().getTime();
+   }
+ }
 
 });
